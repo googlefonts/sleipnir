a7e13ca653e567af946ce3c0b9d57c69cfb3295e
Cosimo Lupo <cosimo@anthrotype.com>
2026-09-18 09:43:05 +0100

    Update tag pushing method in release script
    Same as googlefonts/fontc#2141
    
    JMM

--- resources/scripts/release.sh
+++ resources/scripts/release.sh
@@ -38,7 +38,7 @@ cargo release publish -x ${crate_specifier}  # this prompts y/N
 echo "Generate tags"
 cargo release tag -x ${crate_specifier}  # this prompts y/N
 echo "Pushing tag to github"
-git push --tags
+cargo release push -x ${crate_specifier}  # this prompts y/N
 
 echo "NEXT STEPS"
 echo "You probably want to create a release on github"
